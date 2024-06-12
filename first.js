4b6c7c49ffa6c7b9fcfee1ea5a540690154b9ab4
Jorge Luiz <jorgeluiz1163@gmail.com>
2024-06-12 13:06:19 -0300

    comit inicial

---
+++ first.js
@@ -0,0 +1,3 @@
+//console. log imprime mensagens no terminal (óbvio mas irei anotar de qualquer forma)
+console.log('Hello, World!')
+console.log('Bem vindo ao curso de JavaScript')
---
+++ variaveis.js
@@ -0,0 +1,28 @@
+// VARIÁVEIS são pedaços de memória reservados para guardar determinada informação, tipo uma estante com prateleriras que você guarda as coisas
+
+let idade = 26 //let, variável chamada idade e recebe (=) 26 / let permite mudanças
+
+console.log(idade)
+
+//Tipos de variáveis
+
+idade = 30                       //Variavel do tipo number 
+let numero = 30                   //Variavel do tipo number 
+let altura = 1.77                //Variavel do tipo number 
+let nome = 'Jorge Luiz'          //Variavel do tipo String (texto)
+let estudando = true             // Boolean (recebe apenas true ou false)
+
+console.log(numero, typeof numero) //typeoff imprime o tipo da variável no console
+console.log(altura)
+console.log(nome)
+console.log (estudando)
+
+let outra; //variavel indefinida
+console.log(outra)
+
+var variavel = 30 //var é uma sintaxe antiga, o let substituiu o var
+console.log(variavel)
+
+const constante = 100; //essa variavel é constante, não pode ter o valor alterado
+
+constante = 20;
